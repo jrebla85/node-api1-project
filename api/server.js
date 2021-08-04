b52db376e54c6d07ddcd9fecfe2de4f7e5335657
jeremy black <jrebla85@yahoo.com>
2021-08-04 01:23:07 -0500

    finished POST, and the firt GET endpoints

--- api/server.js
+++ api/server.js
@@ -1,3 +1,34 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Users = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+    const createUser = req.body;
+    if(!createUser.name || !createUser.bio){
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    }else{
+        Users.insert(createUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+    }
+})
+
+server.get("/api/users", (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
