2ff2cc2cd02d6a4396dfc028ce94f09ea2566368
jeremy black <jrebla85@yahoo.com>
2021-08-04 21:58:49 -0500

    Assignment Complete; Bug in the tests, outside of scope of assignment

--- api/server.js
+++ api/server.js
@@ -37,13 +37,13 @@ server.get("/api/users/:id", (req, res) => {
     Users.findById(idVar)
     .then(user => {
         if(!user){
-            res.status(404).json("The user with the specified ID does not exist")
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
         }else{
             res.status(200).json(user)
         }
     })
     .catch(err => {
-        res.status(500).json("The user information could not be retrieved")
+        res.status(500).json({ message: "The user information could not be retrieved" })
     })
 })
 
@@ -61,8 +61,25 @@ server.delete("/api/users/:id", async (req, res) => {
     }
 })
 
-// server.put("/api/users/:id"), (req, res) => {
-    
-// }
+server.put("/api/users/:id", async (req, res) => {
+    try{
+        const {id} = req.params
+        const changes = req.body
+
+        const existingUser = await Users.findById(id)
+        if(!existingUser){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }else{
+            if(!changes.name || !changes.bio){
+                res.status(400).json({ message: "Please provide name and bio for the user" })
+            }else{
+              const updatedUser =  await Users.update(id, changes)
+              res.status(200).json(updatedUser)
+            }
+        }
+    }catch{
+        res.status(500).json({ message: "The user information could not be modified" })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
